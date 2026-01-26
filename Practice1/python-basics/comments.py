fbefab2dd4780f5e1943e538162535190cc68be5
Ayaulym <aaulymkhantarbaj@gmail.com>
2026-01-26 21:03:54 +0500

    Practice1 in progress

---
+++ Practice1/python-basics/comments.py
@@ -0,0 +1,15 @@
+#This is a single-line comment
+print("Hello")
+
+print("Hi") #comment after code
+
+#comment explaining the next line
+x=10
+
+""" 
+This is a multi-line comment (often used as documentation) 
+"""
+print("Python")
+
+#comments
+print("Done")
