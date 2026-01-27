f2f50d9dc23818053a0ac2c0210f976ddfec1947
Ayau <aaulymkhantarbaj@gmail.com>
2026-01-27 17:31:15 +0500

    numbers

--- Practice1/python-basics/numbers.py
+++ Practice1/python-basics/numbers.py
@@ -1,3 +1,4 @@
+#1
 x = 1 #int
 y = 2.8 #float
 z = 1j #complex
@@ -5,3 +6,28 @@ z = 1j #complex
 print(type(x))
 print(type(y))
 print(type(z))
+
+#2
+a = 10
+b = -5
+print(a)
+print(b)
+
+#3
+c = 5       
+d = float(c)
+print(d)
+
+#4
+f = 3 + 5j
+e = 1j
+print(f)
+print(e)
+
+#5
+q = 10
+w = 2.5
+e = 1j
+print(type(q))
+print(type(w))
+print(type(e))
