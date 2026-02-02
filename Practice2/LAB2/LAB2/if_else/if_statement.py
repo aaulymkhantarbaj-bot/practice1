3e99a1579d465446308c5a8aa985000adab376b1
Ayau <aaulymkhantarbaj@gmail.com>
2026-02-02 21:51:01 +0500

    if-else

---
+++ Practice2/LAB2/LAB2/if_else/if_statement.py
@@ -0,0 +1,34 @@
+#1
+a = 33
+b = 200
+
+if b > a:
+  print("b is greater than a")
+
+#2
+number = 15
+if number > 0:
+  print("The number is positive")
+
+#3
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+#4
+age = 25
+is_student = False
+has_discount_code = True
+
+if (age < 18 or age > 65) and not is_student or has_discount_code:
+  print("Discount applies!")
+
+  
+#5
+temperature = 25
+is_sunny = True
+
+if temperature > 20 and is_sunny:
+  print("Perfect beach weather!")
